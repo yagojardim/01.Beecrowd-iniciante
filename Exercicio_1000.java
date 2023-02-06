9e077531707a9de90fbcabf3d065dd1f39d8c573
Yago Jardim <122408245+yagojardim@users.noreply.github.com>
2023-02-06 09:17:30 -0300

    ExerciciosDeFixacao

---
+++ Exercicio_1000.java
@@ -0,0 +1,9 @@
+public class Exercicio_1000 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello World!");	
+
+	}
+
+}
---
+++ Exercicio_1002.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Exercicio_1002 {
+	    
+	public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	     
+	double raio = sc.nextDouble();
+	double PI = 3.14159;
+
+		  
+	double A = PI*(raio*raio);
+		        
+	System.out.printf("A=%.4f%n", A );
+		        
+			
+	sc.close();
+		    }
+		}
